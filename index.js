b70f61badced0f0aeb20cf2cee7dfa917f7d3922
Blanka <szigetiblanka7@gmail.com>
2019-05-22 12:06:22 +0200

    static meg bent

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 
-app.use(express.static('static'));
+app.use(express.static('views'));
 
 // Load routing
 require('./route/index')(app);
+
+app.use((err,req,res,next)=>{
+    res.end('Problem...');
+    console.log(err);
+})
 
 app.listen(3000, function () {
     console.log('Hello :3000');
